90a0f5674ebf2aaf45f723da0fef1573e3a64933
ozguripek <ozgripek@gmail.com>
2020-09-19 23:28:21 +0200

    solutions

--- chapter08/Exercise08_14.java
+++ chapter08/Exercise08_14.java
@@ -1,107 +1,168 @@
 package chapter08;
-
-//GELISTIRILECEK
+import java.util.Scanner;
 
 public class Exercise08_14 {
 
 	public static void main(String[] args) {
-		int[][] matrix = new int[4][4];
-		for (int j = 0; j < 3; j++) {
-			for (int k = 0; k < 3; k++) {
-				matrix[j][k] = (int)(Math.random()*2);
-			}
-		}
-		print(matrix);
-		if (dikeyAyniMi(matrix)==false) {
-			System.out.println("No same numbers on a column");
-			}
-		else {
-			System.out.println("All s on row");
-			}
 		
-		/*int[] dizi=yatayAyniMi(matrix);
-		if (dizi[0]==-1) {
-			System.out.println("No same numbers on a row");
-			}
-		else {
-			System.out.println("All "+ dizi[1]+"s on row"+ dizi[0]);
-			}*/
-		
-	}
-	public static void print(int[][] m) {	
-		for( int i=0; i<3; i++ ){	
-			for (int j = 0; j < 3; j++) {
-				System.out.print(m[i][j] + " ");
-				
-			}	
-		System.out.println();
-		}
-	}
+		int[][] matrix = matrixOlustur();
+		yazdir(matrix);
+		satirKontrolü(matrix);
+		sütunKontrolü(matrix);
+		solÜsttenSagAltaKontrol(matrix);
+		sagÜsttenSolAltaKontrol(matrix);
 
-	public static int[] yatayAyniMi(int[][] matrix) {
-		int[] sayiIndex= new int[2];
-		for (int i = 0; i < matrix.length; i++) {
-			int sifirCount = 0;
-			int birCount = 0;
-			sayiIndex[0]=-1;
-			sayiIndex[1]=-1;
-			for (int j = 1; j < matrix[i].length; j++) {
-				if (matrix[i][j] == 0) {
-					sifirCount++;
-					if(sifirCount==3) {
-					sayiIndex[0]=i;
-					sayiIndex[1]=0;
-					return sayiIndex;
-					}
+	}
+	public static int[][] matrixOlustur() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the size for the matrix: ");
+		int size = input.nextInt();
+		int[][] m = new int[size][size];
+
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m[i].length; j++) {
+				m[i][j] = (int)(Math.random() * 2);
+			}
+		}
+		return m;
+	}
+	
+	public static void yazdir(int[][] m) {
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m[i].length; j++) {
+				System.out.print(m[i][j]);
+			}
+			System.out.println();
+		}
+	}
+	public static void satirKontrolü(int[][] m) {
+		
+		int[] sifirMiBirMi = new int[m.length];
+		for (int i = 0; i < sifirMiBirMi.length; i++) {
+			sifirMiBirMi[i] = -1;
+		}
+
+		int[] sayac = new int[2]; 
+		for (int satir = 0; satir < m.length; satir++) {
+			
+			sayac[0] = 0;
+			sayac[1] = 0;
+
+			for (int sütun = 0; sütun < m[satir].length; sütun++) {
+				sayac[m[satir][sütun]]++;
+				if (sayac[0] < sütun + 1 && sayac[1] < sütun + 1) {//hangi satirda isek sayac o kadar olmali
+					break;
 				}
-				else if((matrix[i][j] == 1)) {
-					birCount++;
-					if(birCount==3) {
-					sayiIndex[0]=i;
-					sayiIndex[1]=1;
-					return sayiIndex;
-					}
+			}
+			if (sayac[0] >= m.length || sayac[1] >= m.length) {
+				if(sayac[0] > sayac[1]) {
+					sifirMiBirMi[satir]=0;
+				}
+				else {
+					sifirMiBirMi[satir]=1;
 				}
 			}
 			
 		}
-		return sayiIndex;
-	}
-
-	public static boolean dikeyAyniMi(int[][] matrix) {
 		
-		for (int i = 0; i < matrix.length; i++) {
-			int sifirCount = 0;
-			int birCount = 0;
-			for (int j = 0; j < matrix[i].length; j++) {
-				if (matrix[j][i] == 0)
-					sifirCount++;
-				if (matrix[j][i] == 1)
-					birCount++;
-			}
-			if (sifirCount == 3||birCount==3)
-				return true;
-		}
-		return false;
-	}
-
-	public static boolean caprazKazandiMi(String[][] matrix, String player) {
-		//Sol yukardan sag asagi
 		int count = 0;
-		for (int i = 0; i < matrix.length; i++) {
-			if (matrix[i][i] == player)
+		for (int i = 0; i < sifirMiBirMi.length; i++) {
+			if (sifirMiBirMi[i] != -1)
+				System.out.println("All " + sifirMiBirMi[i] + "s on row " + (i + 1));
+			else
 				count++;
-			if (count == 3)
-				return true;
+			if (count == sifirMiBirMi.length)
+				System.out.println("No same numbers on a row");
 		}
-		// sag yukardan sol asagi
-		count = 0;
-		for (int i = 0, j = matrix[i].length - 1; j >= 0 ; j--, i++) {
-			if (matrix[i][j] == player)
-				count++;
-			if (count == 3)
-				return true;
-		}
-		return false;
+		
 	}
+	public static void sütunKontrolü(int[][] m) {
+			
+			int[] sifirMiBirMi = new int[m.length];
+			for (int i = 0; i < sifirMiBirMi.length; i++) {
+				sifirMiBirMi[i] = -1;
+			}
+	
+			int[] sayac = new int[2]; 
+			for (int satir = 0; satir < m.length; satir++) {
+				
+				sayac[0] = 0;
+				sayac[1] = 0;
+	
+				for (int sütun = 0; sütun < m[satir].length; sütun++) {
+					sayac[m[sütun][satir]]++;
+					if (sayac[0] < sütun + 1 && sayac[1] < sütun + 1) {//hangi satirda isek sayac o kdar göstermeli
+						break;// yoksa devam etmenin manasi yok
+					}
+				}
+	
+				if (sayac[0] >= m.length || sayac[1] >= m.length) {
+					if(sayac[0] > sayac[1]) {
+						sifirMiBirMi[satir]=0;
+					}
+					else {
+						sifirMiBirMi[satir]=1;
+					}
+				}
+				
+			}
+		
+			int count = 0;
+			for (int i = 0; i < sifirMiBirMi.length; i++) {
+				if (sifirMiBirMi[i] != -1)
+					System.out.println("All " + sifirMiBirMi[i] + "s on column " + (i + 1));
+				else
+					count++;
+				if (count == sifirMiBirMi.length)
+					System.out.println("No same numbers on a column");	
+		
+			}
+	}
+	
+	public static void solÜsttenSagAltaKontrol(int[][] m) {
+		int[] sayac = new int[2]; 
+		int n =-1;
+		sayac[0] = 0;
+		sayac[1] = 0;
+		for (int i = 0; i < m.length; i++) {
+			sayac[m[i][i]]++;
+			if (sayac[0] == m.length)
+				 n=0;
+			if (sayac[1] == m.length)
+				n= 1;
+		}
+		
+		switch (n) {
+			case 0:
+			case 1: System.out.println("All " + n + "s on the major diagonal");
+					  break;
+			case -1: System.out.println("No same numbers on the major diagonal");
+				
+		}
+		
+	}
+	
+	public static void sagÜsttenSolAltaKontrol(int[][] m) {
+		int[] sayac = new int[2];
+		int n=-1;
+		sayac[0] = 0;
+		sayac[1] = 0;
+		for (int i = 0, j = m.length - 1; i < m.length; i++, j--) {
+			sayac[m[i][j]]++;
+			if (sayac[0] == m.length)
+				n= 0;
+			if (sayac[1] == m.length)
+				n= 1;
+		}
+		
+		switch (n) {
+			case 0:
+			case 1: System.out.println("All " + n + "s on the sub-diagonal");
+					  break;
+			case -1: System.out.println("No same numbers on the sub-diagonal");
+				
+		}
+		
+	}
+	
 }
---
+++ chapter08/Exercise08_19.java
@@ -0,0 +1,10 @@
+package chapter08;
+
+public class Exercise08_19 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
