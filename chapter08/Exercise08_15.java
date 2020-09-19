b99985298f48726e54d29603bbae556de3d54860
ozguripek <ozgripek@gmail.com>
2020-09-19 21:34:44 +0200

    solutions

--- chapter08/Exercise08_15.java
+++ chapter08/Exercise08_15.java
@@ -25,7 +25,7 @@ public class Exercise08_15 {
 
 	public static boolean sameLine(double[][] points) {
 		for (int i = 0; i < points.length - 2; i ++) {
-			if (pointPosition(points[i][0], points[i][1], 
+			if (position(points[i][0], points[i][1], 
 					      points[i + 1][0], points[i + 1][1],
 					      points[i + 2][0], points[i + 2][1]) != 0) {
 				return false;
@@ -34,7 +34,7 @@ public class Exercise08_15 {
 		return true;
 	}
 
-	public static double pointPosition(double x0, double y0,
+	public static double position(double x0, double y0,
 	double x1, double y1, double x2, double y2) {
 		
 		double position = ((x1 - x0) * (y2 - y0)) - ((x2 - x0) * (y1 - y0));
--- chapter08/Exercise08_16.java
+++ chapter08/Exercise08_16.java
@@ -3,8 +3,46 @@ package chapter08;
 public class Exercise08_16 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int[][] matrix = {
+						{4, 2},
+						{1, 7},
+						{4, 5},
+						{1, 2},
+						{1, 1},
+						{4, 1}
+						};
+		
+		sort(matrix);
+		
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+		}
+		
 	}
-
+	
+	public static void sort(int m[][]) {
+		int[] min = new int[2];
+		for (int i = 0; i < m.length - 1; i++) {
+				min[0] = m[i][0]; 
+				min[1] = m[i][1]; 
+			int index = i;
+			for (int j = i + 1; j < m.length; j++) {
+				if (m[j][0] <= min[0] && ((m[j][0] < min[1]) || 
+					m[j][1] < min[0] || m[j][1] < min[1])){ 
+					min[0] = m[j][0];
+					min[1] = m[j][1];
+					index = j;
+				}
+			}
+			if (index != i){
+				m[index][0] = m[i][0];
+				m[index][1] = m[i][1];
+				m[i][0] = min[0];
+				m[i][1] = min[1]; 
+			}
+		}
+	}
 }
