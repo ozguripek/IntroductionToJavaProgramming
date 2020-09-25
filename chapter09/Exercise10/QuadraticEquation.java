fd4d87edf19c3c3c0f63f0d8488f19d397d1b139
ozguripek <ozgripek@gmail.com>
2020-09-25 12:48:25 +0200

    solutions

---
+++ chapter09/Exercise10/QuadraticEquation.java
@@ -0,0 +1,51 @@
+package chapter09.Exercise10;
+
+public class QuadraticEquation {
+	private double a, b, c;
+
+	QuadraticEquation(double a,double b, double c) {
+	this.a=a;
+	this.b=b;
+	this.c=c;
+
+	}
+	public double getDiscriminant() {
+		return Math.pow(b, 2)-(4*a*c);
+	}
+	
+	public double getRoot1() {
+			
+		if (getDiscriminant()>0) {
+			return ((-b) + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a); 
+		} 
+		else {
+			return 0; 
+		} 
+	}
+	
+	public double getRoot2() {
+		
+		if (getDiscriminant()>0) {
+			return ((-b) - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a); 
+		} 
+		else {
+			return 0; 
+		} 
+	}
+
+	public double getA() {
+		return a;
+	}
+
+	
+
+	public double getB() {
+		return b;
+	}
+
+
+	public double getC() {
+		return c;
+	}
+	
+}
---
+++ chapter09/Exercise10/TestQuadraticEquation.java
@@ -0,0 +1,31 @@
+package chapter09.Exercise10;
+
+import java.util.Scanner;
+
+public class TestQuadraticEquation {
+	
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);		
+	System.out.println("Enter values for a, b, and c:");
+	double a= input.nextDouble();
+	double b= input.nextDouble();
+	double c= input.nextDouble();
+	
+	QuadraticEquation qE= new QuadraticEquation(a, b,c);
+	
+	System.out.print("The equation has ");
+	if (qE.getDiscriminant() < 0)
+		System.out.println("no real roots");
+	else if (qE.getDiscriminant() > 0) {
+		System.out.println("two roots " + qE.getRoot1() + 
+			" and " + qE.getRoot2());
+	}
+	else {
+		System.out.println("one root " + (qE.getRoot1() > 0 ? 
+				qE.getRoot1() : qE.getRoot2()));
+	}
+	
+
+	}
+
+}
