342cfe583f609cf1624806bc342b8ddca736bbd1
ozguripek <ozgripek@gmail.com>
2020-10-01 21:25:30 +0200

    solutions

---
+++ chapter10/Exercise02/BMITest.java
@@ -0,0 +1,20 @@
+package chapter10.Exercise02;
+
+public class BMITest {
+
+	public static void main(String[] args) {
+		BMI bmi1 = new BMI("Ali", 18, 145, 5, 10);
+		System.out.println("The BMI for " + bmi1.getName() + " is "
+			+ bmi1.getBMI() + " " + bmi1.getStatus());
+
+		BMI bmi2 = new BMI("Veli", 215, 70);
+		System.out.println("The BMI for " + bmi2.getName() + " is "
+			+ bmi2.getBMI() + " " + bmi2.getStatus());
+		
+		BMI bmi3= new BMI ("Mustafa", 20, 87, 50);
+		System.out.println("The BMI for " + bmi3.getName() + " is "
+				+ bmi3.getBMI() + " " + bmi3.getStatus());
+
+	}
+
+}
