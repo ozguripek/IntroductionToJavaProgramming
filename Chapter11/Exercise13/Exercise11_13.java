32b947b156dc68f383653253bae1f9fbfd3b435b
ozguripek <ozgripek@gmail.com>
2020-10-14 01:40:17 +0200

    solutions

---
+++ Chapter11/Exercise13/Exercise11_13.java
@@ -0,0 +1,36 @@
+package Chapter11.Exercise13;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Exercise11_13 {
+
+	public static void main(String[] args) {
+		Scanner input= new Scanner(System.in);
+		
+		ArrayList<Integer> list= new ArrayList<Integer>();
+		
+		System.out.print("Enter 10 integers: ");
+		for (int i = 0; i < 10; i++) {
+			list.add(input.nextInt());
+		}
+		
+		removeDuplicate(list);
+		for(int i=0;i<list.size();i++) {
+			System.out.println(list.get(i)+" ");
+		}
+		//System.out.println();
+	}
+	
+	public static void removeDuplicate(ArrayList<Integer> list) {
+		for(int i=0; i<list.size()-1; i++) {
+			for (int j=i+1; j<list.size();j++) {
+				if(list.get(i)==list.get(j)){
+					list.remove(j);
+				}
+			}
+		}	
+		
+	}
+
+}
