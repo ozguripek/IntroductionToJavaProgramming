6a6b2da2fba8884102168907e9538b886f72480d
ozguripek <ozgripek@gmail.com>
2020-10-17 01:24:52 +0200

    solutions

---
+++ Chapter12/Exercise04/TestLoan.java
@@ -0,0 +1,12 @@
+package Chapter12.Exercise04;
+
+public class TestLoan {
+
+	public static void main(String[] args) {
+		// Loan loan1 = new Loan(0, 5, 5);
+		// Loan loan2 = new Loan(5, 0, 5);
+		 Loan loan3 = new Loan(5, 5, 0);
+	  
+	}
+
+}
