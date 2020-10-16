20281dd49969fbfb6a7211731350a20fbb8cc753
ozguripek <ozgripek@gmail.com>
2020-10-17 00:51:53 +0200

    solutions

---
+++ Chapter12/Exercise02/Exercise12_02.java
@@ -0,0 +1,22 @@
+package Chapter12.Exercise02;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exercise12_02 {
+
+	public static void main(String[] args) throws InputMismatchException {
+		while (true) {
+			try {
+			Scanner input = new Scanner(System.in);
+			System.out.println("Enter two integers");
+			int a = input.nextInt();
+			int b = input.nextInt();
+			System.out.println("sum is: " + (a + b));
+			break;
+			} catch (InputMismatchException ex) {
+				System.out.println("Wrong input");
+			}
+		}
+	}	
+}
