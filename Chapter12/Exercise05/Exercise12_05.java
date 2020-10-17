8ed6dc2e2a74524529e9f925cba86cfca1c1bab0
ozguripek <ozgripek@gmail.com>
2020-10-17 02:00:00 +0200

    solutions

---
+++ Chapter12/Exercise05/Exercise12_05.java
@@ -0,0 +1,13 @@
+package Chapter12.Exercise05;
+
+public class Exercise12_05 {
+
+	public static void main(String[] args) {
+		try {
+		      Triangle t = new Triangle(1, 2, 3);
+		    } catch (Exception e) {
+		      e.printStackTrace();
+		    }
+	}
+
+}
