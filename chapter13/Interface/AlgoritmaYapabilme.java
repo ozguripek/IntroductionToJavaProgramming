c9feb765f62392546434d29dfdd6fa1e61dbe4ce
ozguripek <ozgripek@gmail.com>
2020-10-25 19:40:15 +0100

    solutions

---
+++ chapter13/Interface/AlgoritmaYapabilme.java
@@ -0,0 +1,7 @@
+package chapter13.Interface;
+
+public interface AlgoritmaYapabilme {
+	
+	public void bilgisayarProgramlama();
+
+}
--- chapter13/Interface/BilgisayarYetenegi.java
+++
@@ -1,5 +0,0 @@
-package chapter13.Interface;
-
-public interface BilgisayarYetenegi {
-
-}
